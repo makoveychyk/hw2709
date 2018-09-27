d57e9b5270a0ae27cfa80c902482f5a009ede8c8
Vlad <makoveychyk@gmail.com>
2018-09-27 13:13:17 +0300

    done task 5

--- Task 5/js/myScript.js
+++ Task 5/js/myScript.js
@@ -1,1 +1,11 @@
+//Задание Напишите функцию capitalize(), которая принимает слово из маленьких латинских букв и возвращает его же, меняя первую букву на большую.
+// Например, print(capitalize('hello')) должно печатать слово Hello.
 
+//Решение 
+
+var word = prompt('Введите слово маленькими символами', '');
+function capitalize(word){
+    word = word.charAt(0).toLocaleUpperCase() + word.substr(1, );
+    alert(word);
+}
+capitalize(word);
